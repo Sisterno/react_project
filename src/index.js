000fc17c3009000be14a4e85ef255c952b6f3977
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2019-09-26 16:59:13 -0500

    Guardado por si acaso

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// import './global.css';
+// import App from './components/App';
+
+// const container = document.getElementById('app');
+
+// ReactDOM.render(<App />, container);
+
+    // -----sin react------
+    // const element = document.createElement('h1');
+    // element.innerText = 'Buenas tardes, se;ores de luz';
+
+    // const container = document.getElementById('app');
+
+    // container.appendChild(element);
+    // ------- sin react-------
+
+import React from 'react';// importante - siempre importar las libreias de react para que funcion el .js
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
+// const element = <h1>Prubeas con react</h1>;
+// const element= React.createElement('a',{href: 'https://munisantiago.gob.pe'},'Hol soy los children');
 
-const container = document.getElementById('app');
+const name = 'Gonzalo'
+// const element=React.createElement('h1',{},`Hola soy ${name}`);
+// const jsx = <h1>Hola soy, {name}</h1>;
 
-ReactDOM.render(<App />, container);
+
+const jsx = (<div>
+    <h1>Holas soy {name}</h1>
+    <p>soy ingeniero de sistemas</p>
+</div>);
+
+const container= document.getElementById('app');
+
+
+
+
+
+
+// ReactDOM.render( que, donde)
+ReactDOM.render(jsx,container);
